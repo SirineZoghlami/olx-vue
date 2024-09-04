588b1d71984e37644587b1f9cfdcf2b225feaa2d
SirineZoghlami <sirine.zoghlami@esprit.tn>
2024-09-04 14:24:35 +0100

    first commit

---
+++ src/components/home/main.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h5>Main component</h5>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MainComponent' 
+  }
+  </script>
+  
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+// src/routes.js
+import { createRouter, createWebHistory } from 'vue-router';
+import Main from './components/home/main.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Main',
+    component: Main,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
